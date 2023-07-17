269dea332301d094bf44f37d350e35920eb23bf5
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-07-17 01:12:39 -0300

    feat: improves send commit data function (and get commit message & commit hash by args)

--- project-example/sendCommitData.js
+++ project-example/sendCommitData.js
@@ -9,15 +9,36 @@ const MAIN_APP_URL = 'http://localhost:4000';
  */
 async function sendCommitData(commitMessage, commitHash) {
   try {
+    if(!commitMessage) {
+      throw new Error('Missing "commitMessage".');
+    }
+
+    if(!commitHash) {
+      throw new Error('Missing "commitHash".');
+    }
+
     const commitData = { commitMessage, commitHash };
+
+    const shortCommitMessage = commitMessage.length > 25 ? commitMessage.substring(0, 25).concat('...') : commitMessage
+    console.info(`Submitting commit ${commitHash} (${shortCommitMessage})`);
 
     await axios.post(`${MAIN_APP_URL}/commits`, commitData);
 
     console.info(`Commit ${commitHash} submitted successfully.`);
   } catch (err) {
     console.error(`An error occurred when submitting commit ${commitHash}:`);
-    console.error(err);
+
+    let finalErrorDetails = err;
+
+    if(err instanceof axios.AxiosError) {
+      finalErrorDetails = err.response?.data || err.cause;
+    }
+
+    console.error(finalErrorDetails);
   }
 }
 
-module.exports = sendCommitData;
+const commitMessage = process.argv[2];
+const commitHash = process.argv[3];
+
+sendCommitData(commitMessage, commitHash);
