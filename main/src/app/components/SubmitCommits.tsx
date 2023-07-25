96716d5e0293831ceec4552b7c2cd822a0476a0d
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-07-24 22:14:02 -0300

    chore: add tooltip feedback

--- main/src/app/components/SubmitCommits.tsx
+++ main/src/app/components/SubmitCommits.tsx
@@ -4,9 +4,17 @@ import { SubmitCommitsForm } from './forms/SubmitCommitsForm'
 import { useCommitsFilterStore } from '@/stores/CommitsFilterStore'
 import { cn } from '@/lib/utils'
 import { useCommitsStore } from '@/stores/CommitsStore'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 
 export function SubmitCommits() {
+  const [mounted, setMounted] = useState(false)
+
   const submitStatus = useCommitsFilterStore((s) => s.submitStatus)
   const byUsername = useCommitsFilterStore((s) => s.byUsername)
 
@@ -31,36 +39,63 @@ export function SubmitCommits() {
     [unsubmittedCommitsOfUser, commitIdsSelected],
   )
 
-  return (
-    <div
-      className={cn('space-y-6', {
-        'pointer-events-none select-none opacity-30 dark:opacity-20':
-          submitStatus !== 'not-submitted' || !byUsername,
-      })}
-    >
-      <header>
-        <h1 className="whitespace-nowrap text-2xl font-semibold text-zinc-500 dark:text-zinc-300">
-          Submit Commits
-        </h1>
+  useMemo(() => {
+    setMounted(true)
+  }, [])
 
-        <div className="flex items-center gap-1.5">
-          <span className="text-xs opacity-40">
-            {unsubmittedCommitsSelected.length} unsubmitted commit
-            {unsubmittedCommitsSelected.length > 1 ? 's' : ''}
-          </span>
+  const children = useMemo(
+    () => (
+      <div
+        className={cn('space-y-6', {
+          'pointer-events-none select-none opacity-30 dark:opacity-20':
+            submitStatus !== 'not-submitted' || !byUsername,
+        })}
+      >
+        <header>
+          <h1 className="whitespace-nowrap text-start text-2xl font-semibold text-zinc-500 dark:text-zinc-300">
+            Submit Commits
+          </h1>
 
-          {unsubmittedCommitsOfUser.length !==
-            unsubmittedCommitsSelected.length && (
-            <span className="text-[10px] opacity-30 dark:opacity-20">
-              {`of ${unsubmittedCommitsOfUser.length} unsubmitted commits in total`}
+          <div className="flex items-center gap-1.5">
+            <span className="text-xs opacity-40">
+              {unsubmittedCommitsSelected.length} unsubmitted commit
+              {unsubmittedCommitsSelected.length > 1 ? 's' : ''}
             </span>
-          )}
-        </div>
-      </header>
 
-      <SubmitCommitsForm
-        unsubmittedCommitsSelected={unsubmittedCommitsSelected}
-      />
-    </div>
+            {unsubmittedCommitsOfUser.length !==
+              unsubmittedCommitsSelected.length && (
+              <span className="text-[10px] opacity-30 dark:opacity-20">
+                {`of ${unsubmittedCommitsOfUser.length} unsubmitted commits in total`}
+              </span>
+            )}
+          </div>
+        </header>
+
+        <SubmitCommitsForm
+          unsubmittedCommitsSelected={unsubmittedCommitsSelected}
+        />
+      </div>
+    ),
+    [
+      byUsername,
+      unsubmittedCommitsOfUser.length,
+      unsubmittedCommitsSelected,
+      submitStatus,
+    ],
+  )
+
+  if (!mounted || byUsername) {
+    return children
+  }
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger className="cursor-default">{children}</TooltipTrigger>
+        <TooltipContent>
+          You should only submit commits from one person (preferably you)
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
